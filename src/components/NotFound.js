326c3779c359788ce5b868a3822a3ba640496b52
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-09-13 14:24:09 -0400

    Updated 404 error component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <div className="main-content not-found">
+  <div className="main-nav not-found">
     <h2>Page Not Found</h2>
-    <iframe src="https://giphy.com/embed/jkZtSdwKOx05BOlapR" width="480" height="307" frameBorder="0" class="giphy-embed" allowFullScreen title="404 Error" />
+    <Link to="/" > Return Home </Link>
+    <iframe src="https://giphy.com/embed/jkZtSdwKOx05BOlapR" width="480" height="307" frameBorder="0" className="giphy-embed" allowFullScreen title="404 Error" />
     {/* <p><a href="https://giphy.com/gifs/nehumanesociety-dog-space-keyboard-jkZtSdwKOx05BOlapR">via GIPHY</a></p>*/}
-    <Link to="/"> Return Home </Link>
   </div>
 );
 
